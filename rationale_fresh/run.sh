1f37f80f9c7bccf2fd5f74eb2e35c7777f7379cc
Chao-Chun Hsu <joe32140@hotmail.com.tw>
2020-12-06 22:43:13 -0700

    result analysis

--- rationale_fresh/run.sh
+++ rationale_fresh/run.sh
@@ -3,7 +3,7 @@ for data in pheme_drop_unverified pheme_all
 do
     for rationale_ratio in 0.5 0.3
     do
-        for thresholder in contiguous
+        for thresholder in top_k #contiguous
         do
             CUDA_DEVICE=0 \
             OUTPUT_DIR=/data/joe/misinformation/FRESH/output/ \
@@ -15,7 +15,7 @@ do
             MAX_LENGTH_RATIO=$rationale_ratio \
             SALIENCY=wrapper \
             THRESHOLDER=$thresholder \
-            EPOCHS=3 \
+            EPOCHS=10 \
             BSIZE=8 \
             HUMAN_PROB=0 \
             bash Rationale_Analysis/commands/fresh/fresh_with_extractor_script.sh
